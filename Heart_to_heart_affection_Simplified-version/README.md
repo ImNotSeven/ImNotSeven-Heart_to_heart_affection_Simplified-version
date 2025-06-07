2eda1aa1296359c77f8c7c05b9a63ae196b8dc7a
ImNotSeven <161426343+ImNotSeven@users.noreply.github.com>
2025-06-07 17:47:47 +0800

    Delete Heart_to_heart_affection_Simplified-version directory

--- Heart_to_heart_affection_Simplified-version/README.md
+++
@@ -1,6 +0,0 @@
-#Heart_to_heart_affection
-
-MySQL数据库
-IP 8.130.165.3
-用户名 petcommunity
-密码 123456
--- Heart_to_heart_affection_Simplified-version/cart/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "cart"
-urlpatterns = [
-    # 其他URL配置...
-    #path('cart/add_cart/',views.add_cart)
-]
